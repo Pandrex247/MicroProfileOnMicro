dfdd4e0fbedc7276902d7082618e19a8a49b64ba
Andrew Pielage <pandrex247@hotmail.com>
2019-09-25 15:09:53 +0100

    Add extra OpenAPI example

--- src/main/java/fish/payara/examples/microprofileonmicro/openapi/OpenApiEndpoints.java
+++ src/main/java/fish/payara/examples/microprofileonmicro/openapi/OpenApiEndpoints.java
@@ -3,7 +3,9 @@ package fish.payara.examples.microprofileonmicro.openapi;
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 
 /**
  *
@@ -14,9 +16,15 @@ import org.eclipse.microprofile.openapi.annotations.Operation;
 public class OpenApiEndpoints {
     
     @GET
-    @Path("/Wobbles")
-    @Operation(operationId = "Wibbles", summary = "Wibs", description = "Wibbles to and fro")
-    public String wobbles() {
+    @Path("/Wobbly")
+    @Operation(operationId = "Wibbly", summary = "Wibs", description = "Wibbles to and fro")
+    public String wobbly() {
         return "Wibbly Wobbly!";
+    }
+    
+    @GET
+    @Path("Tiddles/{Toddles}")
+    public String tiddles(@Parameter(description = "What does Tiddles do?", example = "Giggles") @PathParam("Toddles") String toddles) {
+        return "Tiddles " + toddles + "!";
     }
 }
